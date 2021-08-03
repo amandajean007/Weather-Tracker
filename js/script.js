89ca0be3d16ea1deff7bc7c8ea7c9ea9a3e18d99
Amanda McBee <ahanes35@yahoo.com>
2021-08-03 12:33:49 -0600

    mid-project backup

--- js/script.js
+++ js/script.js
@@ -1,89 +1,202 @@
-// Add todays date
+//------------- Adds todays date-----------------------------------------------------------------------------------------------------------------//
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
 
 
-// Creates variables to attach Javascript commands to places in the HTML document
-  // In the header where todays date will be shown
-var today = document.getElementById("todaysWeather");
-  // Form where you type in the city to be searched, it grabs the info you type in with ".value"
-var cityInput = document.getElementById("cityForm").value;
-  // The button bellow the search city input box
-var getWeatherButton = document.getElementById("getWeather");
-  // Bottom Left Box where cities searched will be listed
-var cityList = document.getElementById("myCities");
-  // The object where the list of searched cities will be placed
+//--------------Adds city inputs to Local Storage and then re-places them back on the webpage ---------------------------------------------------//
+var cityInput = document.querySelector("#city-text");
+var cityForm = document.querySelector("#city-form");
+var cityList = document.querySelector("#city-list");
+var getWeatherButton = document.querySelector("#getWeather");
 var cities = [];
-  // Storing my API key in a variable
-var APIKey = "7c89fcf5fc9da80f2ff12bfa557dbb49";
-  // Variable calling the API
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&appid="+APIKey;
+
+function rendercities() {
+
+  cityList.innerHTML = "";
 
 
-// Pulls cities from local storage with key="City " to add to the recently searched cities list in the bottom left box of the webpage
-function putCitiesOnPage() {
-  cityList.textContent = localStorage.getItem("City ");
-  // Takes in user input and makes it into an element and creates the list item and the complete button and pushes onto DOM
   for (var i = 0; i < cities.length; i++) {
-    var City = cities[i];
-        // creates list item and pushes to the DOM
-        var ul = document.createElement("ul");
-        ul.textContent = City;
-        ul.setAttribute("data-index", i);
-        // Creates element to be pushed onto the DOM
-        var button = document.createElement("button");
-        button.textContent = "Remove City";
-        // Adds buttons and cities to the list
-        ul.appendChild(button);
-        cityList.appendChild(ul);
-      };
-    }
+    var city = cities[i];
 
-// Adds Inputed City "value" into local storage with key "City ". Then puts them back on the page with function putCitiesOnPage().
-function setCityWeather() {
-    localStorage.setItem("City ", cityInput);
-    putCitiesOnPage();
-}
+    var ul = document.createElement("ul");
+    ul.textContent = city;
+    ul.setAttribute("data-index", i);
 
-//Retrieves information from the API
-function getWeather(queryURL) {
-  fetch(queryURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
+    var button = document.createElement("button");
+    button.textContent = "Remove";
+
+    ul.appendChild(button);
+    cityList.appendChild(ul);
+  }
 }
 
 function init() {
-  var storedCities = JSON.parse(localStorage.getItem("Cities"));
-  if (storedCities !== null) {
-    cities = storedCities;
+  var storedcities = JSON.parse(localStorage.getItem("cities"));
+
+  if (storedcities !== null) {
+    cities = storedcities;
   }
-  putCitiesOnPage();
+
+  rendercities();
 }
 
-function storeCities() {
-  localStorage.setItem("Cities", JSON.stringify(cities));
+function storecities() {
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
 getWeatherButton.addEventListener("click", function(event) {
   event.preventDefault();
-  var cityText = cityInput;
+
+  var cityText = cityInput.value;
+
+
   if (cityText === "") {
     return;
   }
+
   cities.push(cityText);
-  cityInput = "";
-  storeCities();
-  putCitiesOnPage();
+  cityInput.value = "";
+
+
+  storecities();
+  rendercities();
 });
 
 cityList.addEventListener("click", function(event) {
   var element = event.target;
+
+  // Checks if element is a button
   if (element.matches("button") === true) {
+
     var index = element.parentElement.getAttribute("data-index");
     cities.splice(index, 1);
-    storeCities();
-    putCitiesOnPage();
+
+    storecities();
+    rendercities();
   }
 });
 
 init();
+
+
+/*
+var stateInput = document.querySelector("#state-text");
+var stateForm = document.querySelector("#state-form");
+var stateList = document.querySelector("#state-list");
+var states = [];
+
+function renderstates() {
+
+  stateList.innerHTML = "";
+
+
+  for (var i = 0; i < states.length; i++) {
+    var state = states[i];
+
+    var ul = document.createElement("ul");
+    ul.textContent = state;
+    ul.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Remove";
+
+    ul.appendChild(button);
+    stateList.appendChild(ul);
+  }
+}
+
+function init() {
+  var storedstates = JSON.parse(localStorage.getItem("states"));
+
+  if (storedstates !== null) {
+    states = storedstates;
+  }
+
+  renderstates();
+}
+
+function storestates() {
+  localStorage.setItem("states", JSON.stringify(states));
+}
+
+getWeatherButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var stateText = stateInput.value;
+
+  if (stateText === "") {
+    return;
+  }
+
+  states.push(stateText);
+  stateInput.value = "";
+
+  storestates();
+  renderstates();
+});
+
+stateList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+
+    var index = element.parentElement.getAttribute("data-index");
+    states.splice(index, 1);
+
+    storestates();
+    renderstates();
+  }
+});
+
+init()
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------Posts todays weather forecast API call-----------------------------------------------------------------------------------------------//
+
+var todaysWeather = document.getElementById("todaysWeather");
+  // Storing my API key in a variable
+var APIKey = "7c89fcf5fc9da80f2ff12bfa557dbb49";
+
+// Defines a function that retrieves information from the API using fetch
+function getWeather() {
+  // Variable holding the API call
+  var city = document.getElementById(cityInput);
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+",US&appid="+APIKey;
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = data[i].html_url;
+      todaysWeather.appendChild(listItem);
+      console.log(data);
+    }
+  });
+}
+
+// When the Get Weather button is clicked on, 
+getWeatherButton.addEventListener("click", getWeather);
