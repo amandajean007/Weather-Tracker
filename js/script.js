b556546609b2d2b6c11a0c6552ef7fa2a16e8b00
Amanda McBee <ahanes35@yahoo.com>
2021-08-05 22:35:30 -0600

    got some data to show up on page

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
 
-git 
+ 
 //--------------Adds city inputs to Local Storage and then re-places them back on the webpage ---------------------------------------------------//
 var cityInput = document.querySelector("#city-text");
 var cityForm = document.querySelector("#city-form");
 var cityList = document.querySelector("#city-list");
 var getWeatherButton = document.querySelector("#getWeather");
 var cities = [];
+var cityName = document.querySelector("#cityName");
+var city = document.querySelector("#city");
+var temp = document.querySelector("#temp");
+var desc = document.querySelector("#desc");
+var humidity = document.querySelector("#humidity");
+var icon = document.querySelector("#weather-icon");
+var uvIndex = document.querySelector("#uvIndex");
+
 
 function rendercities() {
 
@@ -60,21 +68,8 @@ getWeatherButton.addEventListener("click", function(event) {
     })
     .then(function (data) {
       console.log(data);
-      //Loop over the data to generate a table, each table row will have a link to the request URL
-      for (var i=0; i< data.length; i++) {
-        // Creating elements: tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url
-
-        //Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
+        show(data);
+    
     });
 
   if (cityText === "") {
@@ -84,9 +79,61 @@ getWeatherButton.addEventListener("click", function(event) {
   cities.push(cityText);
   cityInput.value = "";
 
+  function show(data) {
+    console.log(data.main.temp);
+    temp.textContent = data.main.temp;
+    cityName.textContent = data.name;
+    icon.textContent = data.weather[0].icon;
+    wind.textContent = data.wind.speed;
+    humidity.textContent = data.main.humidity;
+  };
+
   storecities();
   rendercities();
-});
+ });
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+getWeatherButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  // creates a variable to take the city input and add it to the API request
+  var cityText = cityInput.value;
+  // Stores my API key in a variable
+  var APIKey = "49f3f0a393b9b135b6d926bc4536144b";
+  // Stores the API call in a variable
+  var queryUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q="+cityText+"&cnt={5}&appid="+APIKey;
+
+  // fetch API call
+  fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      show(data);
+    });
+
+  if (cityText === "") {
+    return;
+  }
+
+  cities.push(cityText);
+  cityInput.value = "";
+
+  function show(data) {
+    console.log(data);
+    forecastDate1.textContent = data.
+
+    /*temp.textContent = data.main.temp;
+    cityName.textContent = data.name;
+    icon.textContent = data.weather[0].icon;
+    wind.textContent = data.wind.speed;
+    humidity.textContent = data.main.humidity;*/
+  };
+
+  storecities();
+  rendercities();
+ });
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Will remove the city from the list when you click "remove"
 cityList.addEventListener("click", function(event) {
@@ -104,41 +151,3 @@ cityList.addEventListener("click", function(event) {
 });
 
 init();
-
-//---------Posts todays weather forecast API call-----------------------------------------------------------------------------------------------//
-/*
-var todaysWeather = document.getElementById("todaysWeather");
-
-function getApi() {
-  // Stores my API key in a variable
-  var APIKey = "49f3f0a393b9b135b6d926bc4536144b";
-  // Stores the API call in a variable
-  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&appid="+APIKey;
-
-  fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      //Loop over the data to generate a table, each table row will have a link to the request URL
-      for (var i=0; i< data.length; i++) {
-        // Creating elements: tablerow, tabledata, and anchor
-        var creeateTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
-
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url
-
-        //Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableData.appendChild(link);
-        creeateTableRow.appendChild(tableData);
-        tableBody.appendChild(creeateTableRow); 
-      }
-    });
-};
-
-getWeatherButton.addEventListener("click", getApi()); */
